9b713d4246e809edbb1740944207d6d48e75edf3
Ilham Dohaan <doha39anilham@gmail.com>
2022-09-13 17:11:23 +0700

    initial commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.main')
+
+@section('container')
+<h1>Ini Halaman About</h1>
+<h3>{{$name}}</h3>
+<p>{{$email}}</p>
+<img src= "img/{{$image}}" alt="thor" width="150px">
+@endsection
